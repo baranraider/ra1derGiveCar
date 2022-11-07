70c9dd72a8061dd4063349240e001cd28930d286
ra1der <73917011+baranraider@users.noreply.github.com>
2022-11-07 20:35:48 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -28,3 +28,43 @@ RegisterServerEvent("giveSpawnVehicle", function(vehicleProps, vehicle)
 	end)
 end)
 
+--NEW QBCORE İçin
+--local function GeneratePlate()
+-- 	local plate = QBCore.Shared.RandomInt(1) .. QBCore.Shared.RandomStr(2) .. QBCore.Shared.RandomInt(3) .. QBCore.Shared.RandomStr(2)
+-- 	local result = MySQL.scalar.await('SELECT plate FROM player_vehicles WHERE plate = ?', {plate})
+-- 	if result then
+-- 		return GeneratePlate()
+-- 	else
+-- 		return plate:upper()
+-- 	end
+-- end
+
+-- QBCore.Functions.CreateCallback("ra1der:givecar:check", function(source, cb, args)
+-- 	local Player = QBCore.Functions.GetPlayer(tonumber(args.input))
+-- 	if Player == nil then
+-- 		cb(false) 
+-- 	else
+-- 		TriggerClientEvent('spawnVehicle:client', tonumber(args.input), args.input2, GeneratePlate(), true)
+-- 		cb(true)
+-- 	end
+-- end)
+
+-- RegisterServerEvent("giveCar", function(vehicleProps, vehiclemodel, vehicle)
+-- 	local pData = QBCore.Functions.GetPlayer(source)
+-- 	MySQL.insert('INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, garage, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', {
+-- 		pData.PlayerData.license,
+-- 		pData.PlayerData.citizenid,
+-- 		vehiclemodel,
+-- 		GetHashKey(vehiclemodel),
+-- 		json.encode(vehicleProps),
+-- 		vehicleProps.plate,
+-- 		'',
+-- 		0
+-- 	}, function() 
+-- 		TriggerClientEvent("QBCore:Notify", pData.PlayerData.source, vehicleProps.plate.." plakalı araç artık senin")
+-- 	end)
+-- end)
+
+
+
+
